bcf8c5ba929963343fd1e650b656938ea79f57dc
Arthur Legrix <alegrix@c1r3p4.42lehavre.fr>
2024-11-19 05:51:16 +0100

    swap + push

--- utils/pushFunctions.c
+++ utils/pushFunctions.c
@@ -6,7 +6,7 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 23:33:56 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/19 03:43:14 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/19 05:19:57 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 void	push(t_list **first, t_list **second, char x)
 {
-	t_list	*new_m;
+	t_list	*temp;
 
-	if (x == 'a')
-		ft_printf("pa\n");
-	else
-		ft_printf("pb\n");
-	if (ft_lstsize(*first) == 0)
+	if (!*first)
 		return ;
-	new_m = ft_lstnew((*first)->content);
-	free(*first);
-	ft_lstadd_front(second, new_m);
+	*temp = *second;
+	*second = *first;
+	*first = *first.next;
+	*second.next = temp;
+}
+
+void	pa(t_list **a, t_list **b)
+{
+	ft_printf("pa\n");
+	push(&a, &b);
+}
+
+void	pb(t_list **a, t_list **b)
+{
+	ft_printf("pa\n");
+	push(&b, &a);
 }
--- utils/swapFunctions.c
+++ utils/swapFunctions.c
@@ -6,31 +6,38 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 23:24:04 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/19 03:21:43 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/19 05:50:44 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	swap(t_list **lst, char x)
+void	swap(t_list **lst)
 {
-	int temp;
+	t_list	*temp;
 
-	if (x == 'a')
-		ft_printf("sa\n");
-	else if (x == 'b')
-		ft_printf("sb\n");
 	if (ft_lstsize(*lst) < 2)
 		return ;
-	temp = (*lst)->content;
-	(*lst)->content = (*lst)->next->content;
-	(*lst)->next->content = temp;
-	ft_lstprint(*lst);
+	temp = *lst->next;
+	(*lst)->next = (*lst)->next->next;
+	*lst = temp;
 }
 
-void	double_swap(t_list **lst_a, t_list **lst_b)
+void	ss(t_list **lst_a, t_list **lst_b)
 {
-	swap(lst_a, 'x');
-	swap(lst_b, 'x');
+	swap(&lst_a, 'x');
+	swap(&lst_b, 'x');
 	ft_printf("ss\n");
+}
+
+void	sa(t_list **lst)
+{
+	ft_printf("sa\n");
+	swap(&lst);
+}
+
+void    sb(t_list **lst)
+{
+	ft_printf("sb\n");
+	swap(&lst);
 }
